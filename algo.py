a1500f8ccde8367b40d4513c46c617154a302294
Austin Hochman <a.w.hochman@gmail.com>
2021-11-05 16:35:11 -0400

    Update algo.py

--- algo.py
+++ algo.py
@@ -1,67 +1,78 @@
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
 import yfinance as yf
 from datetime import datetime
 import backtrader as bt
 import backtrader.feeds as btfeeds
 import backtrader.indicators as btind
-from clustering import Clustering
 
+class SuperTrendBand(bt.Indicator):
+    """
+    Helper inidcator for Supertrend indicator
+    """
+    params = (('period',7),('multiplier',3))
+    lines = ('basic_ub','basic_lb','final_ub','final_lb')
 
-class SupportResistence(bt.SignalStrategy):
-
-        self.order_id = None
 
     def __init__(self):
-        self.order_id = None
-        self.status = 0
-        self.portfolio_value = 100000
-        self.support = 300
-        self.resistence = 400 
-        self.margin = .2
-        self.clustering = Clustering()
-        self.exchange_amt = .2
-        self.pp = btind.PivotPoint(self.data)
-        self.pp.plotinfo.plot = True
-
-    def notify_order(self, order):
-        if order.status in [bt.Order.Submitted, bt.Order.Accepted]:
-            return
-        
-        self.order_id = None
+        self.atr = bt.indicators.AverageTrueRange(period=self.p.period)
+        self.l.basic_ub = ((self.data.high + self.data.low) / 2) + (self.atr * self.p.multiplier)
+        self.l.basic_lb = ((self.data.high + self.data.low) / 2) - (self.atr * self.p.multiplier)
 
     def next(self):
-        # self.support, self.resistence = Clustering.update()
-        cur_margin = (self.resistence - self.support) * self.margin
-        # print(cur_margin, self.data0.close[0], self.resistence - cur_margin)
+        if len(self)-1 == self.p.period:
+            self.l.final_ub[0] = self.l.basic_ub[0]
+            self.l.final_lb[0] = self.l.basic_lb[0]
+        else:
+            #=IF(OR(basic_ub<final_ub*,close*>final_ub*),basic_ub,final_ub*)
+            if self.l.basic_ub[0] < self.l.final_ub[-1] or self.data.close[-1] > self.l.final_ub[-1]:
+                self.l.final_ub[0] = self.l.basic_ub[0]
+            else:
+                self.l.final_ub[0] = self.l.final_ub[-1]
 
-        if self.order_id:
+            #=IF(OR(baisc_lb > final_lb *, close * < final_lb *), basic_lb *, final_lb *)
+            if self.l.basic_lb[0] > self.l.final_lb[-1] or self.data.close[-1] < self.l.final_lb[-1]:
+                self.l.final_lb[0] = self.l.basic_lb[0]
+            else:
+                self.l.final_lb[0] = self.l.final_lb[-1]
+
+class SuperTrend(bt.Indicator):
+    """
+    Super Trend indicator
+    """
+    params = (('period', 7), ('multiplier', 3))
+    lines = ('super_trend',)
+    plotinfo = dict(subplot=False)
+
+    def __init__(self):
+        self.stb = SuperTrendBand(period = self.p.period, multiplier = self.p.multiplier)
+
+    def next(self):
+        if len(self) - 1 == self.p.period:
+            self.l.super_trend[0] = self.stb.final_ub[0]
             return
 
-        if (self.data0.close[0] > self.resistence - cur_margin) and (self.status != 1):
-            self.sell(data=self.data0, size=(self.broker.getvalue() * self.exchange_amt))
-            self.status = 1
-        
-        if (self.data0.close[0] < self.support + cur_margin) and (self.status != 2):
-            self.buy(data=self.data0, size=(self.broker.getvalue() * self.exchange_amt))
-            self.status = 2
-        
+        if self.l.super_trend[-1] == self.stb.final_ub[-1]:
+            if self.data.close[0] <= self.stb.final_ub[0]:
+                self.l.super_trend[0] = self.stb.final_ub[0]
+            else:
+                self.l.super_trend[0] = self.stb.final_lb[0]
 
-    def stop(self):
-        print(f'Initial portfolio value: {self.broker.startingcash}')
-        print(f'Final   portfolio value: {self.broker.getvalue()}')
-        print(
-            f'Return             rate: {self.broker.getvalue()/self.broker.startingcash}'
-        )
-
+        if self.l.super_trend[-1] == self.stb.final_lb[-1]:
+            if self.data.close[0] >= self.stb.final_lb[0]:
+                self.l.super_trend[0] = self.stb.final_lb[0]
+            else:
+                self.l.super_trend[0] = self.stb.final_ub[0]
 
 def main():
     cerebro = bt.Cerebro()
 
     ma = bt.feeds.PandasData(
-        dataname=yf.download('MA', datetime(2021, 1, 1), datetime(2021, 6, 10)))
+        dataname=yf.download('MA', datetime(2011, 1, 1), datetime(2021, 6, 10)))
 
     cerebro.adddata(ma)
 
-    cerebro.addstrategy(SupportResistence)
+    cerebro.addstrategy(SuperTrend)
 
     cerebro.run()
     cerebro.plot()
