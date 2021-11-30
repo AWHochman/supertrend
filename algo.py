c8fd11a325fe4da1824fd8e2a6bcdd00966b1f48
Austin Hochman <a.w.hochman@gmail.com>
2021-11-30 16:43:16 -0500

    Added obv line

--- algo.py
+++ algo.py
@@ -14,8 +14,9 @@ class SuperTrendStrategy(bt.SignalStrategy):
         self.order_id = None
         self.status = 0
         self.portfolio_value = 100000
-        self.exchange_amt = .5
+        self.exchange_amt = .7
         self.st = SuperTrend(period=7, multiplier=3)
+        self.obv = OnBalanceVolume(self.data)
         self.ema3 = EMA(self.data, period=3)
         self.ema20 = EMA(self.data, period=20)
 
@@ -29,6 +30,8 @@ class SuperTrendStrategy(bt.SignalStrategy):
 
         if self.order_id:
             return
+        
+        # print(self.obv[0])
 
         if (self.data.close > self.st) and (self.ema3 < self.ema20) and (self.status != 1):
             numShares = (self.broker.getvalue() - self.broker.getcash())/self.data.close
@@ -109,13 +112,55 @@ class SuperTrend(bt.Indicator):
             else:
                 self.l.super_trend[0] = self.stb.final_ub[0]
 
+
+class OnBalanceVolume(bt.Indicator):
+
+    alias = 'OBV'
+    lines = ('obv',)
+
+    plotlines = dict(
+        obv=dict(
+            _name='OBV',
+            color='purple',
+            alpha=0.50
+        )
+    )
+
+    def __init__(self):
+
+        self.plotinfo.plotyhlines = [0]
+
+    def nextstart(self):
+        c = self.data.close
+        v = self.data.volume
+        obv = self.lines.obv
+
+        if c[0] > c[-1]:
+            obv[0] = v[0]
+
+        elif c[0] < c[-1]: 
+            obv[0] = -v[0] 
+        else: 
+            obv[0] = 0 
+        
+    def next(self):  
+        c = self.data.close 
+        v = self.data.volume 
+        obv = self.lines.obv 
+        if c[0] > c[-1]:
+            obv[0] = obv[-1] + v[0]
+        elif c[0] < c[-1]:
+            obv[0] = obv[-1] - v[0]
+        else:
+            obv[0] = obv[-1]
+
 def main():
     cerebro = bt.Cerebro()
 
     # ma = bt.feeds.PandasData(
     #     dataname=yf.download('TSLA', datetime(2018, 1, 1), datetime(2021, 1, 1)))
     ma = bt.feeds.PandasData(
-        dataname=yf.download('AAPL', datetime(2011, 1, 1), datetime(2020, 1, 1)))
+        dataname=yf.download('MSFT', datetime(2019, 1, 1), datetime(2021, 1, 1)))
 
     cerebro.adddata(ma)
 
